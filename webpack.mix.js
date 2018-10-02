1e575e57854aec00df9276bce2d9d9b8b4f95bd5
Dave Shoreman <dshoreman@users.noreply.github.com>
2018-10-02 03:28:30 +0100

    Enable browserSync for auto-refreshing fun

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .copy('node_modules/semantic-ui-css/semantic.min.css', 'public/css/')
+   .browserSync('localhost:8000')
