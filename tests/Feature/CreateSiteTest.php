f0891405054f917525da4bda5772a82884091e85
Dave Shoreman <dshoreman@users.noreply.github.com>
2018-10-11 04:15:08 +0100

    Add tests for validation of site creation

--- tests/Feature/CreateSiteTest.php
+++ tests/Feature/CreateSiteTest.php
@@ -37,4 +37,57 @@ class CreateSiteTest extends TestCase
         $this->assertEquals('http://example.com', $site->primary_domain);
         $this->assertTrue($site->is_enabled);
     }
+
+    public function testNameIsRequired()
+    {
+        $response = $this->postJson('/api/sites', ['name' => '']);
+
+        $this->assertValidationErrors($response, 'name');
+
+        $this->assertNull(Site::first());
+    }
+
+    public function testNameMustBeString()
+    {
+        $response = $this->postJson('/api/sites', ['name' => 42]);
+
+        $this->assertValidationErrors($response, 'name');
+
+        $this->assertNull(Site::first());
+    }
+
+    public function testNameMustBeUnique()
+    {
+        Site::create(['name' => 'Duplicate me!']);
+
+        $response = $this->postJson('/api/sites', ['name' => 'Duplicate me!']);
+
+        $this->assertValidationErrors($response, 'name');
+
+        $this->assertEquals(1, Site::count());
+    }
+
+    public function testPrimaryDomainMustBeUrl()
+    {
+        $response = $this->postJson('/api/sites', [
+            'name' => 'Good name',
+            'primary_domain' => 'not a url',
+        ]);
+
+        $this->assertValidationErrors($response, 'primary_domain');
+
+        $this->assertNull(Site::first());
+    }
+
+    public function testIsEnabledMustBeBoolean()
+    {
+        $response = $this->postJson('/api/sites', [
+            'name' => 'Good name',
+            'is_enabled' => 'true',
+        ]);
+
+        $this->assertValidationErrors($response, 'is_enabled');
+
+        $this->assertNull(Site::first());
+    }
 }
