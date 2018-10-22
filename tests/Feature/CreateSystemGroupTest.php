6eba4744a017f89c778963713f0818c4ff1004d7
Dave Shoreman <dshoreman@users.noreply.github.com>
2018-10-22 14:46:00 +0100

    Add group creation tests based on manpage caveats
    That is, the CAVEATS section of `man groupadd` that specifies guidelines
    for group name restrictions. The tests added cover Debian's constriants.

---
+++ tests/Feature/CreateSystemGroupTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateSystemGroupTest extends TestCase
+{
+    /** @test */
+    public function nameCannotStartWithDash()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => '-test-dash-prefix',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function nameCannotStartWithPlus()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => '+test-plus-prefix',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function nameCannotStartWithTilde()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => '~test-tilde-prefix',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function nameCannotContainColon()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => 'test-contains-:',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function nameCannotContainComma()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => 'test-contains-,',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function nameCannotContainTab()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => "test-contains-\t",
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function nameCannotContainNewline()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => "test-contains-\n",
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function nameCannotContainWhitespace()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => 'test-contains- ',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function nameCannotBeTooLong()
+    {
+        $response = $this->postJson('/api/system/groups', [
+            'name' => '_im-a-name-that-is-over-32-chars',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
