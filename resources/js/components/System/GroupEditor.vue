e18a2d9f4bac10ff550c1f5a40e24374b273fb45
Dave Shoreman <dshoreman@users.noreply.github.com>
2018-11-16 01:47:33 +0000

    Indentation fix

--- resources/js/components/System/GroupEditor.vue
+++ resources/js/components/System/GroupEditor.vue
@@ -1,39 +1,39 @@
 <template>
-        <sui-form @submit.prevent="editMode ? updateGroup(tmpGroup.id_original) : addGroup()">
-            <sui-form-fields>
-                <sui-form-field width="ten">
-                    <label>Name</label>
-                    <input v-model="tmpGroup.name" ref="name" placeholder="group-name">
-                </sui-form-field>
-                <sui-form-field width="six">
-                    <label>GID</label>
-                    <input v-model="tmpGroup.id" type="number">
-                </sui-form-field>
-            </sui-form-fields>
-            <sui-button-group fluid>
-                <sui-button type="button" @click="reset()">Cancel</sui-button>
-                <sui-button-or></sui-button-or>
-                <sui-button type="submit" positive :content="editMode ? 'Update' : 'Create'" />
-            </sui-button-group>
+    <sui-form @submit.prevent="editMode ? updateGroup(tmpGroup.id_original) : addGroup()">
+        <sui-form-fields>
+            <sui-form-field width="ten">
+                <label>Name</label>
+                <input v-model="tmpGroup.name" ref="name" placeholder="group-name">
+            </sui-form-field>
+            <sui-form-field width="six">
+                <label>GID</label>
+                <input v-model="tmpGroup.id" type="number">
+            </sui-form-field>
+        </sui-form-fields>
+        <sui-button-group fluid>
+            <sui-button type="button" @click="reset()">Cancel</sui-button>
+            <sui-button-or></sui-button-or>
+            <sui-button type="submit" positive :content="editMode ? 'Update' : 'Create'" />
+        </sui-button-group>
 
-            <div v-if="tmpGroup.users.length">
-                <sui-header size="small" v-show="editMode">Group Members</sui-header>
-                <sui-list divided relaxed>
-                    <sui-list-item v-for="user in tmpGroup.users" :key="user">
-                        <sui-list-icon name="user" size="large" vertical-align="middle" />
-                        <sui-list-content>
-                            <sui-list-header>{{ user }}</sui-list-header>
-                        </sui-list-content>
-                    </sui-list-item>
-                </sui-list>
-            </div>
+        <div v-if="tmpGroup.users.length">
+            <sui-header size="small" v-show="editMode">Group Members</sui-header>
+            <sui-list divided relaxed>
+                <sui-list-item v-for="user in tmpGroup.users" :key="user">
+                    <sui-list-icon name="user" size="large" vertical-align="middle" />
+                    <sui-list-content>
+                        <sui-list-header>{{ user }}</sui-list-header>
+                    </sui-list-content>
+                </sui-list-item>
+            </sui-list>
+        </div>
 
-            <sui-header size="small" v-show="editMode">Danger Zone</sui-header>
-            <sui-segment class="red" v-show="editMode">
-                <sui-button negative icon="trash" type="button"
-                    content="Delete Group" @click="deleteGroup(tmpGroup.id)" />
-            </sui-segment>
-        </sui-form>
+        <sui-header size="small" v-show="editMode">Danger Zone</sui-header>
+        <sui-segment class="red" v-show="editMode">
+            <sui-button negative icon="trash" type="button"
+                content="Delete Group" @click="deleteGroup(tmpGroup.id)" />
+        </sui-segment>
+    </sui-form>
 </template>
 
 <script>
