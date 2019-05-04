438aff8c5385f6e7499a11490418e0516f26080b
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-05-04 05:35:22 +0100

    Simplify var references in SiteItem component

--- resources/js/components/Sites/SiteItem.vue
+++ resources/js/components/Sites/SiteItem.vue
@@ -1,11 +1,11 @@
 <template>
     <sui-card>
         <sui-card-content>
-            <sui-card-header>{{ site.name }}</sui-card-header>
-            <sui-card-meta>{{ site.primary_domain }}</sui-card-meta>
+            <sui-card-header>{{ name }}</sui-card-header>
+            <sui-card-meta>{{ primary_domain }}</sui-card-meta>
         </sui-card-content>
         <router-link is="sui-button" attached="bottom"
-            :to="{ name: 'apps.edit', params: { id: site.id } }">
+            :to="{ name: 'apps.edit', params: { id } }">
             <sui-icon name="cogs"></sui-icon> Manage Site
         </router-link>
     </sui-card>
@@ -15,6 +15,13 @@
 export default {
     props: {
         site: Object,
+    },
+    data() {
+        return {
+            id: this.site.id,
+            name: this.site.name,
+            primary_domain: this.site.primary_domain,
+        };
     },
 }
 </script>
