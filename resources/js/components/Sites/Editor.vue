815c4adbf6d9b82aa2036b1c25891869a0d22419
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-05-05 10:43:42 +0100

    Add dummy form with essential fields
    Updates title, replaces Back button with Cancel/Save and adds fields for
    the App name and its primary domain.

--- resources/js/components/Sites/Editor.vue
+++ resources/js/components/Sites/Editor.vue
@@ -1,12 +1,29 @@
 <template>
     <sui-container>
         <h2 is="sui-header">
-            {{ site.name }}
+            Editing {{ site.name }}
             <sui-header-subheader>
                 {{ site.primary_domain }}
             </sui-header-subheader>
         </h2>
-        <router-link is="sui-button" :to="{ name: 'apps' }" content="Back" />
+
+        <sui-form>
+            <sui-form-field>
+                <label>App Name</label>
+                <sui-input v-model="site.name" placeholder="My Blog" />
+            </sui-form-field>
+            <sui-form-field>
+                <label>Primary Domain</label>
+                <sui-input v-model="site.primary_domain" placeholder="example.com" />
+            </sui-form-field>
+
+            <sui-button-group>
+                <router-link :to="{ name: 'apps' }" is="sui-button"
+                    type="button" content="Cancel" />
+                <sui-button-or />
+                <sui-button type="submit" positive content="Save" />
+            </sui-button-group>
+        </sui-form>
     </sui-container>
 </template>
 
