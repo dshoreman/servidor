4314a00a3d0d8d9d6289e0017d0fbb4400b71736
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-07-30 20:12:57 +0100

    Create symlink when site is enabled (#63)
    When a site is updated, if it's enabled and a symlink doesn't already
    exist with the same name, we now create it before restarting nginx.
    
    Still need to cleanup when disabled and handle real files when a symlink
    is expected, but this should be enough to get it working for now.

--- app/Listeners/WriteSiteConfig.php
+++ app/Listeners/WriteSiteConfig.php
@@ -23,10 +23,18 @@ class WriteSiteConfig
         $filename = $site->primary_domain.'.conf';
         $filepath = 'vhosts/'.$filename;
         $fullpath = Storage::disk('local')->path($filepath);
+        $confpath = '/etc/nginx/sites-available/'.$filename;
+        $livepath = '/etc/nginx/sites-enabled/'.$filename;
 
         $template = 'laravel' == $site->type ? 'php' : $site->type;
 
         Storage::put($filepath, view('sites.server-templates.'.$template, ['site' => $site]));
-        exec('sudo cp "'.$fullpath.'" "/etc/nginx/sites-available/'.$filename.'"');
+        exec('sudo cp "'.$fullpath.'" "'.$confpath.'"');
+
+        if ($site->is_enabled && !is_link($livepath)) {
+            exec('sudo ln -s "'.$confpath.'" "'.$livepath.'"');
+        }
+
+        exec('sudo systemctl reload-or-restart nginx.service');
     }
 }
