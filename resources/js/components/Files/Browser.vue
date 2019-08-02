8d7e9346f65067bb4dea61f0d317458364c89b6f
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-08-02 06:37:58 +0100

    Split file browser into components

---
+++ resources/js/components/Files/Browser.vue
@@ -0,0 +1,17 @@
+<template>
+    <sui-container>
+        <h2>/</h2>
+
+        <file-list />
+    </sui-container>
+</template>
+
+<script>
+import FileList from './Browser/FileList';
+
+export default {
+    components: {
+        FileList,
+    },
+}
+</script>
---
+++ resources/js/components/Files/Browser/FileList.vue
@@ -0,0 +1,51 @@
+<template>
+    <sui-table compact>
+        <thead>
+            <tr>
+                <th>Filename</th>
+                <th>Permissions</th>
+                <th>Owner</th>
+                <th>Group</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <sui-table-cell collapsing>
+                    <sui-icon name="folder" /> bin -> usr/bin
+                </sui-table-cell>
+                <td>lrwxrwxrwx</td>
+                <td>root</td>
+                <td>root</td>
+            </tr>
+            <tr>
+                <sui-table-cell collapsing>
+                    <sui-icon name="folder" /> etc
+                </sui-table-cell>
+                <td>drewr-xr-x</td>
+                <td>root</td>
+                <td>root</td>
+            </tr>
+            <tr>
+                <sui-table-cell collapsing>
+                    <sui-icon name="folder" /> home
+                </sui-table-cell>
+                <td>drwxr-xr-x</td>
+                <td>root</td>
+                <td>root</td>
+            </tr>
+            <tr>
+                <sui-table-cell collapsing>
+                    <sui-icon name="folder" /> var
+                </sui-table-cell>
+                <td>drwxr-xr-x</td>
+                <td>root</td>
+                <td>root</td>
+            </tr>
+        </tbody>
+    </sui-table>
+</template>
+
+<script>
+export default {
+}
+</script>
