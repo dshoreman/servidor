a8000f7536580e3aee3504d37424f24f95791c3f
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-08-21 14:33:35 +0100

    Switch site cards to using text for Manage links
    While we're using a button attached to the bottom of the card, there
    won't be enough space to add an extra button for browsing files.
    
    By putting a plain link inside the extra content container, it both
    looks cleaner and sticks to the left creating plenty of space.

--- resources/js/components/Sites/SiteItem.vue
+++ resources/js/components/Sites/SiteItem.vue
@@ -4,10 +4,12 @@
             <sui-card-header>{{ site.name }}</sui-card-header>
             <sui-card-meta>{{ site.primary_domain }}</sui-card-meta>
         </sui-card-content>
-        <router-link is="sui-button" attached="bottom"
-            :to="{ name: 'apps.edit', params: { id: site.id } }">
-            <sui-icon name="cogs"></sui-icon> Manage Site
-        </router-link>
+
+        <sui-card-content extra>
+            <router-link :to="{ name: 'apps.edit', params: { id: site.id } }">
+                <sui-icon name="cogs"></sui-icon> Manage Site
+            </router-link>
+        </sui-card-content>
     </sui-card>
 </template>
 
