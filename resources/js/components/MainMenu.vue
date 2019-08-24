2a87a5c7ee82c937f8bd8e06e181b8bc9b10ef4f
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-08-24 21:35:24 +0100

    Merge System menu into the main sidebar menu
    Removes the old System layout and menu components and updates the
    users/groups pages to be standalone rather than children of a /system
    route, since we don't want to end up with a duplicated layout.
    
    Still isn't perfect, seems to be a bit of spacing below the submenu so
    that could do with being re-done at some point, but meh.

--- resources/js/components/MainMenu.vue
+++ resources/js/components/MainMenu.vue
@@ -1,10 +1,26 @@
 <template>
     <sui-menu class="large" secondary fluid inverted vertical pointing v-if="use_new">
-        <router-link :to="{ name: item.href }" is="sui-menu-item"
-            v-for="(item, id) in menu" :key="id" :active="isActive(item)">
+        <router-link :to="{ name: item.href }" is="sui-menu-item" v-if="!item.menu"
+            v-for="item in menu" :key="item.href" :active="isActive(item)">
             {{ item.name }}
         </router-link>
+
+        <template v-else>
+            <sui-menu-item class="link" :active="isActive(item)">
+
+                <sui-menu-header :content="item.name" />
+
+                <sui-menu secondary fluid inverted vertical pointing>
+                    <router-link :to="{ name: subitem.href }" is="sui-menu-item" :active="isActive(subitem, true)"
+                        v-for="subitem in item.menu" :key="subitem.href" class="submenu-item">
+                        {{ subitem.name }}
+                    </router-link>
+                </sui-menu>
+
+            </sui-menu-item>
+        </template>
     </sui-menu>
+
     <sui-menu :widths="menu.length" v-else>
         <router-link :to="{ name: item.href }" is="sui-menu-item"
             v-for="(item, id) in menu" :key="id" :active="isActive(item)">
@@ -31,12 +47,23 @@ export default {
                 href: 'files'
             }, {
                 name: 'System',
-                href: 'system.groups'
+                href: 'system.groups',
+                menu: [{
+                    name: 'Groups',
+                    href: 'system.groups',
+                }, {
+                    name: 'Users',
+                    href: 'system.users',
+                }],
             }],
         };
     },
     methods: {
-        isActive (item) {
+        isActive (item, absolute = false) {
+            if (absolute) {
+                return this.$route.path == '/' + item.href.replace('.', '/');
+            }
+
             let href = '/' + item.href.split('.')[0];
 
             return this.$route.path.startsWith(href);
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -5,7 +5,6 @@ import Sites from './pages/Sites.vue'
 import SiteList from './pages/Sites/List.vue'
 import SiteEditor from './pages/Sites/Edit.vue'
 import NewLayout from './layouts/Servidor.vue'
-import SystemLayout from './layouts/System.vue'
 import SystemGroups from './components/System/Groups.vue'
 import SystemUsers from './components/System/Users.vue'
 import Login from './pages/Auth/Login.vue'
@@ -56,18 +55,15 @@ const routes = [{
             path: route.params.path ? route.params.path : '/var/www'
         }),
     }, {
-        path: '/system', component: SystemLayout,
-        children: [{
-            component: SystemGroups,
-            name: 'system.groups',
-            path: '/system/groups',
-            meta: { auth: true },
-        }, {
-            component: SystemUsers,
-            name: 'system.users',
-            path: '/system/users',
-            meta: { auth: true },
-        }],
+        component: SystemGroups,
+        name: 'system.groups',
+        path: '/system/groups',
+        meta: { auth: true },
+    }, {
+        component: SystemUsers,
+        name: 'system.users',
+        path: '/system/users',
+        meta: { auth: true },
     }],
 }, {
     component: Login,
