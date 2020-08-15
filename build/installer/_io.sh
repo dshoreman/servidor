162fc69896995f8cb8023f17d789a13da5de2d89
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-08-15 23:07:55 +0100

    Rewrite edit_line function for better reliability
    tl;dr: .env should now be much cleaner, and any existing values should
    now be reliably handled to prevent them overriding the saved settings.
    
    ---
    
    Various different values were causing issues with the previous code used
    in the Vagrant bootstrap script, particularly involving "special" chars
    used in passwords which would cause the sed patterns to break.
    
    This completely reworks the function so that we first check for an exact
    match and remove *all* other exact or empty matches, which both cleans
    up duplicates in the .env and reduces load in the second loop.
    
    If we don't find an exact match, the second loop will find the first
    line that matches the option regardless of whether it has a value or
    not. If it does we comment and append like, otherwise edit in-place as
    we did previously. Once a value is set, all further matches will be
    either commented or removed depending on whether a value is defined.
    
    Along with a vastly different set of sed (and now awk) commands, we now
    have two cleansing functions that escape values for the left (safe_srch)
    or right (safe_repl) side of the sed given pattern. This should help to
    avoid the majority of previous issues caused by values with symbols in.

--- build/installer/_io.sh
+++ build/installer/_io.sh
@@ -40,16 +40,55 @@ success() {
 }
 
 edit_line() {
-    local file="$1" match="$2" replace="$3"
+    local file="$1" match="$2" replace="$3" found val sVal sRep
 
-    # Bail out if exact match is found
-    grep -qP "^${match}=${replace}$" "${file}" && return
-
-    if grep -qP "^${match}=$" "${file}"; then
-        # Option is unset, edit line in-place
-        sed -i "s/^\(${match}\)=$/\1=${replace}/" "$file"
-    else
-        # Option is already set, comment it and append new line
-        sed -i "s/^\(\(${match}\)=.\+\)$/#\1\n\2=${replace}/" "$file"
+    log "Setting ${match} to ${replace} in ${file}..."
+    if ! grep -qP "^${match}=.*$" "${file}"; then
+        echo "${match}=${replace}" >> "${file}"
+        log " Done! No matches were found, so we added our own."
+        return
     fi
+
+    log " Existing values found, checking for exact match..."
+    while IFS= read -r val; do
+        if [ "$val" = "=$replace" ]; then
+            found="true"
+            log "  Perfect, found a match! Deleting any duplicates..." && \
+                awk -v line="^${match}${val}$" -v prefix="^${match}=$" \
+                    '($0 !~ line || ++n == 1) && $0 !~ prefix' "$file" \
+                    > "/tmp/${file}.servitmp" && mv "/tmp/${file}.servitmp" "$file"
+            break
+        fi
+    done < <(grep -oP "(?<=^${match})=.+" "${file}")
+
+    while IFS= read -r val; do
+        sVal="$(safe_srch "${val}")"
+        sRep="$(safe_repl "${replace}")"
+
+        if [ -n "$found" ] && [ "$val" = "=" ]; then
+            log "  Deleting extraneous '${match}=' line..."
+            sed -i "0,/^${match}=$/{//d}" "$file"
+            found="true"; continue
+        elif [ -n "$found" ]; then
+            [ "$val" = "=${replace}" ] && continue
+            log "  Commenting existing non-empty setting..."
+            sed -i "0,/^\(${match}${sVal}\)$/{s//# \1/}" "$file"
+            found="true"; continue
+        elif [ "$val" = "=" ]; then
+            log "  First match is empty! Editing line in-place..."
+            sed -i "0,/^\(${match}=\)$/{s//\1${sRep}/}" "$file"
+            found="true"; continue
+        fi
+
+        log "  Commenting first non-empty match, and adding our own..."
+        sed -i "0,/^\(\(${match}\)${sVal}\)$/{s//# \1\n\2=${sRep}/}" "$file"
+        found="true"
+    done < <(grep -oP "(?<=^${match})=.*" "${file}")
+}
+
+safe_srch() {
+    printf '%s\n' "${*}" | sed -e 's/[]\/$*.^[]/\\&/g'
+}
+safe_repl() {
+    printf '%s\n' "${*}" | sed -e 's/[\/&]/\\&/g'
 }
