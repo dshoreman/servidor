4fa941df9a9470d30fa0b128008444695287e77a
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-12-23 01:44:38 +0000

    Move app domain and template to a sidebar

--- resources/js/pages/Projects/ProjectViewer.vue
+++ resources/js/pages/Projects/ProjectViewer.vue
@@ -6,62 +6,71 @@
             {{ project.name }}
         </h2>
 
-        <div v-for="app in project.applications" :key="app.id">
-            <h3 is="sui-header">
-                <sui-icon :name="appIcon.name" :color="appIcon.color" />
-                <sui-header-content>
-                    {{ app.domain_name }}
-                    <sui-header-subheader>
-                        {{ app.template }}
-                    </sui-header-subheader>
-                </sui-header-content>
-            </h3>
+        <sui-grid>
+            <sui-grid-row>
+                <sui-grid-column :width="3">
+                    <sui-list divided relaxed style="margin-top: 10px;">
+                        <sui-list-item v-for="app in project.applications" :key="app.id">
+                            <sui-list-icon :name="appIcon.name"
+                                size="large" :color="appIcon.color" />
+                            <sui-list-content>
+                                <a is="sui-list-header">{{ app.domain_name }}</a>
+                                <a is="sui-list-description">{{ app.template }}</a>
+                            </sui-list-content>
+                        </sui-list-item>
+                    </sui-list>
+                </sui-grid-column>
 
-            <sui-header attached="top" :inverted="darkMode">Source Files</sui-header>
-            <sui-segment attached :inverted="darkMode">
-                <sui-grid>
-                    <sui-grid-row :columns="2">
-                        <sui-grid-column>
-                            <sui-header size="tiny" :inverted="darkMode">
-                                Repository
-                                <sui-header-subheader v-if="app.source_repository">
-                                    <sui-icon :name="app.source_provider" />
-                                    <span>{{ app.source_repository }}</span>
-                                </sui-header-subheader>
-                                <sui-header-subheader v-else>
-                                    No source repository has been set for this project!
+                <sui-grid-column :width="13">
+                    <div v-for="app in project.applications" :key="app.id">
+                        <sui-header attached="top" :inverted="darkMode">Source Files</sui-header>
+                        <sui-segment attached :inverted="darkMode">
+                            <sui-grid>
+                                <sui-grid-row :columns="2">
+                                    <sui-grid-column>
+                                        <sui-header size="tiny" :inverted="darkMode">
+                                            Repository
+                                            <sui-header-subheader v-if="app.source_repository">
+                                                <sui-icon :name="app.source_provider" />
+                                                <span>{{ app.source_repository }}</span>
+                                            </sui-header-subheader>
+                                            <sui-header-subheader v-else>
+                                                No source repository has been set for this project!
+                                            </sui-header-subheader>
+                                        </sui-header>
+                                    </sui-grid-column>
+                                    <sui-grid-column>
+                                        <sui-header size="tiny" :inverted="darkMode">
+                                            Tracking Branch
+                                            <sui-header-subheader v-if="app.source_branch">
+                                                {{ app.source_branch }}
+                                            </sui-header-subheader>
+                                            <sui-header-subheader v-else>
+                                                Using default branch
+                                            </sui-header-subheader>
+                                        </sui-header>
+                                    </sui-grid-column>
+                                </sui-grid-row>
+                            </sui-grid>
+
+                            <sui-header size="tiny" :inverted="darkMode" v-if="app.project_root">
+                                <router-link :to="{ name: 'files', params: { path: app.project_root } }"
+                                             content="Browse files" is="sui-button" floated="right"
+                                             basic primary icon="open folder" />
+                                Project Root
+                                <sui-header-subheader>{{ app.project_root }}</sui-header-subheader>
+                            </sui-header>
+                            <sui-header size="tiny" :inverted="darkMode" v-else>
+                                Project Root
+                                <sui-header-subheader>
+                                    This project doesn't have a project root defined.
                                 </sui-header-subheader>
                             </sui-header>
-                        </sui-grid-column>
-                        <sui-grid-column>
-                            <sui-header size="tiny" :inverted="darkMode">
-                                Tracking Branch
-                                <sui-header-subheader v-if="app.source_branch">
-                                    {{ app.source_branch }}
-                                </sui-header-subheader>
-                                <sui-header-subheader v-else>
-                                    Using default branch
-                                </sui-header-subheader>
-                            </sui-header>
-                        </sui-grid-column>
-                    </sui-grid-row>
-                </sui-grid>
-
-                <sui-header size="tiny" :inverted="darkMode" v-if="app.project_root">
-                    <router-link :to="{ name: 'files', params: { path: app.project_root } }"
-                                 content="Browse files" is="sui-button" floated="right"
-                                 basic primary icon="open folder" />
-                    Project Root
-                    <sui-header-subheader>{{ app.project_root }}</sui-header-subheader>
-                </sui-header>
-                <sui-header size="tiny" :inverted="darkMode" v-else>
-                    Project Root
-                    <sui-header-subheader>
-                        This project doesn't have a project root defined.
-                    </sui-header-subheader>
-                </sui-header>
-            </sui-segment>
-        </div>
+                        </sui-segment>
+                    </div>
+                </sui-grid-column>
+            </sui-grid-row>
+        </sui-grid>
 
     </div>
 
