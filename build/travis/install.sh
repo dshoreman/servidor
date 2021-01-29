44e8a9da8b340d527f1419545e53ec8de4ab99fe
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-01-29 23:22:34 +0000

    Set travis to test in php 8.0 instead of 7.3

--- build/travis/install.sh
+++ build/travis/install.sh
@@ -21,11 +21,7 @@ php_install() {
 
     cp build/travis/dotenv ./.env
     git config --global pull.ff only
-    if [[ "${TRAVIS_PHP_VERSION:0:3}" == "7.3" ]]; then
-        travis_retry composer install -n --ignore-platform-req=php
-    else
-        travis_retry composer install --no-interaction
-    fi
+    travis_retry composer install --no-interaction
 
     php artisan key:generate
     php artisan migrate && php artisan passport:install
