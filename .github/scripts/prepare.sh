a32a73414d449507e23ce4aadf8c9356dd2d5b0c
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-02-02 19:58:16 +0000

    Fix permissions in FileManager tests

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -8,9 +8,12 @@ main() {
     sudo mkdir -p /var/www /etc/nginx/sites-enabled /etc/nginx/sites-available
     sudo chmod -R 777 /etc/nginx /var/www
 
-    sudo chmod 777 "${skeleton}"
     sudo chown -R www-data:www-data "${skeleton}"
     sudo chown -R root:root "${skeleton}/protected"
+
+    sudo chmod 777 "${skeleton}"
+    sudo chmod 775 "${skeleton}"/mixed/another-dir
+    sudo chmod 664 "${skeleton}"/mixed/hello.md "${skeleton}"/hidden/.bar
     sudo chmod 600 "${skeleton}/protected/forbidden"
 }
 
