d5ed63a712b8bf203082681def116c4ccbfd8375
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-05-05 19:26:27 +0100

    Configure phpinsights' rules to match the project
    * Allow short ternary operator
    * Allow newlines in bracketed expressions
    * Configure max line lengths
    * Disable "space after not" sniff
    * Disable class element ordering
    * Invert the yoda rule
    
    This also excludes Rule classes from unused checks due to the surplus
    but unavoidable `$attribute` parameter passed to rules by Laravel.
    FileManager is excluded for the `$_` in exception, but can be fixed.
    
    The "no silenced errors" sniff has to be disabled in FileManager despite
    already having `@SuppressWarnings()` in the docblock for PHPMD. Using
    inline `// phpcs:ignore` format also gets ignored, unfortunately.
    
    Finally, we remove use-sorting and useless doc comment sniffs from the
    blacklist since they're not errorring so no reason not to have them.

--- build/phpinsights/config.php
+++ build/phpinsights/config.php
@@ -5,16 +5,24 @@ declare(strict_types=1);
 use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenDefineFunctions;
 use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenFinalClasses;
 use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenNormalClasses;
-use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenPrivateMethods;
 use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenTraits;
 use NunoMaduro\PhpInsights\Domain\Metrics\Architecture\Classes;
-use SlevomatCodingStandard\Sniffs\Commenting\UselessFunctionDocCommentSniff;
-use SlevomatCodingStandard\Sniffs\Namespaces\AlphabeticallySortedUsesSniff;
+use NunoMaduro\PhpInsights\Domain\Metrics\Code\Code;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Formatting\SpaceAfterNotSniff;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\NoSilencedErrorsSniff;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\WhiteSpace\ArbitraryParenthesesSpacingSniff;
+use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
+use SlevomatCodingStandard\Sniffs\ControlStructures\DisallowShortTernaryOperatorSniff;
+use SlevomatCodingStandard\Sniffs\ControlStructures\DisallowYodaComparisonSniff;
+use SlevomatCodingStandard\Sniffs\ControlStructures\RequireYodaComparisonSniff;
+use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\DisallowMixedTypeHintSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\PropertyTypeHintSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff;
+use SlevomatCodingStandard\Sniffs\Variables\UnusedVariableSniff;
 
 return [
     'preset' => 'laravel',
@@ -27,10 +35,13 @@ return [
         Classes::class => [
             ForbiddenFinalClasses::class,
         ],
+        Code::class => [
+            RequireYodaComparisonSniff::class,
+        ],
     ],
 
     'remove' => [
-        AlphabeticallySortedUsesSniff::class,
+        // Default Config
         DeclareStrictTypesSniff::class,
         DisallowMixedTypeHintSniff::class,
         ForbiddenDefineFunctions::class,
@@ -39,12 +50,39 @@ return [
         ParameterTypeHintSniff::class,
         PropertyTypeHintSniff::class,
         ReturnTypeHintSniff::class,
-        UselessFunctionDocCommentSniff::class,
+
+        // Manual Overrides
+        DisallowShortTernaryOperatorSniff::class,
+        DisallowYodaComparisonSniff::class,
+        OrderedClassElementsFixer::class,
+        SpaceAfterNotSniff::class,
     ],
 
     'config' => [
-        ForbiddenPrivateMethods::class => [
-            'title' => 'The usage of private methods is not idiomatic in Laravel.',
+        ArbitraryParenthesesSpacingSniff::class => [
+            'ignoreNewlines' => true,
+        ],
+        LineLengthSniff::class => [
+            'lineLimit' => 100,
+            'absoluteLineLimit' => 110,
+        ],
+        NoSilencedErrorsSniff::class => [
+            'exclude' => [
+                'app/FileManager/FileManager.php',
+            ],
+        ],
+        UnusedParameterSniff::class => [
+            'exclude' => [
+                'app/Rules/Domain.php',
+                'app/Rules/NoComma.php',
+                'app/Rules/NoColon.php',
+                'app/Rules/NoWhitespace.php',
+            ],
+        ],
+        UnusedVariableSniff::class => [
+            'exclude' => [
+                'app/FileManager/FileManager.php',
+            ],
         ],
     ],
 
