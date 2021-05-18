d531d5b3ec1ab11a2071494a31382acda0af422e
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-05-18 04:08:03 +0100

    Add PHP CS Fixer to husky pre-commit script

---
+++ build/hooks/pre-commit.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+cEnd=$'\e[0m'
+cOrange=$'\e[39m'
+cGreen=$'\e[36m'
+cRed=$'\e[31m'
+cBlue=$'\e[34m'
+
+csFixerBin=./vendor/bin/php-cs-fixer
+stagedProjectFiles=$(git diff-index --cached --name-only --diff-filter=ACMR HEAD -- )
+changedInstallFiles=$(git diff --cached --name-only | grep 'build/installer/' | wc -l | xargs)
+
+if [[ changedInstallFiles -gt 0 ]]; then
+    echo "${cOrange}Detected change in install scripts, rebuilding installer!${cEnd}"
+
+    if make installer && git add setup.sh; then
+        echo "${cGreen}setup.sh updated.${cEnd}"
+        echo
+    else
+        echo "${cRed}Installer build failed, aborting commit.${cEnd}"
+        echo && exit 1
+    fi
+fi
+
+if [[ ! -f "$csFixerBin" ]] || [[ ! -x "$csFixerBin" ]]; then
+    echo "${cOrange}PHP CS Fixer is not installed or executable. Attempting composer install...${cEnd}"
+
+    if ! composer install -q; then
+        echo "${cRed}Composer install failed. PHP CS fixes skipped.${cEnd}"
+        echo && exit 0
+    fi
+fi
+
+if [[ -n "${stagedProjectFiles}" ]]; then for file in $stagedProjectFiles; do
+    if echo "$file" | egrep -q "\.(php)$"; then
+        if ! php -l -d display_errors=0 "$file"; then
+            echo "${cRed} File ${file} contains syntax errors.${cEnd}"
+            echo && exit 2;
+        fi
+
+        echo "${cGreen} Fixing file ${file}...${cEnd}"
+
+        result=$( $csFixerBin fix --config-file=build/cs-fixer/config.php $file )
+        echo "$result" | sed -e "s/\(.*\)/$cBlue\1$cEnd/g; s/\+  /$cGreen+  /g; s/-  /$cRed-  /g;"
+        git add --patch "$file"
+    fi
+done; fi
