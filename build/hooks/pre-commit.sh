a6f01f581e2c713fafd59a595ff2b0ed7da5cc34
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-05-25 03:09:56 +0100

    Second attempt at auto-fix
    Rather than stashing, fixing and popping files, the idea behind this
    method is to fix *only* those files that are completely staged, ie with
    no additional changes in the working tree prior to fixing.
    
    We still have to stash so that we can identify whether the files to be
    committed will need fixing, and not just some unstaged file we don't
    care about. If we reset anything that's left after adding all the "safe"
    fixes to the commit, the stash can be popped entirely free of conflicts.

--- build/hooks/pre-commit.sh
+++ build/hooks/pre-commit.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 cEnd=$'\e[0m'
-cOrange=$'\e[39m'
-cGreen=$'\e[36m'
+cOrange=$'\e[33m'
+cGreen=$'\e[32m'
 cRed=$'\e[31m'
 cBlue=$'\e[34m'
 
 csFixerBin=./vendor/bin/php-cs-fixer
-stagedProjectFiles=$(git diff-index --cached --name-only --diff-filter=ACMR HEAD -- )
-changedInstallFiles=$(git diff --cached --name-only | grep 'build/installer/' | wc -l | xargs)
+changedInstallFiles=$(git diff-index --cached --name-only HEAD | grep -c 'build/installer/')
 
 if [[ changedInstallFiles -gt 0 ]]; then
     echo "${cOrange}Detected change in install scripts, rebuilding installer!${cEnd}"
@@ -31,19 +30,58 @@ if [[ ! -f "$csFixerBin" ]] || [[ ! -x "$csFixerBin" ]]; then
     fi
 fi
 
-if [[ -n "${stagedProjectFiles}" ]]; then
-    let phpFiles=0
-    for file in $stagedProjectFiles; do if echo "$file" | egrep -q "\.(php)$"; then
+mapfile -t stagedPhpFiles < <(git diff-index --cached --name-only --diff-filter=ACMR HEAD | grep -E '\.(php)$')
+if [[ ${#stagedPhpFiles[@]} -gt 0 ]]; then
+    declare -a safeFiles
+    stashed=false
+
+    echo "${cBlue} Checking ${#stagedPhpFiles[@]} files for syntax errors${cEnd}"
+    for file in "${stagedPhpFiles[@]}"; do
         if ! php -l -d display_errors=0 "$file"; then
             echo "${cRed} File ${file} contains syntax errors.${cEnd}"
             echo && exit 2;
         fi
-        phpFiles+=1
-    fi; done
+        if git diff --quiet --name-only "${file}"; then
+            # Working copy is clean so file is fully staged and safe to fix
+            safeFiles+=("${file}")
+        fi
+    done
 
-    if [[ phpFiles -gt 0 ]]; then
-        echo "${cGreen} Fixing files...${cEnd}"
-        result=$( $csFixerBin fix --config=build/php-cs-fixer/config.php )
-        echo "$result" | sed -e "s/\(.*\)/$cBlue\1$cEnd/g; s/\+  /$cGreen+  /g; s/-  /$cRed-  /g;"
+    # Start by loading unstaged changes in PHP files
+    mapfile -t unstagedPhpFiles < <(git diff --name-only | grep -E '\.(php)$')
+
+    # So we can stash the changes we don't want to commit
+    if [[ ${#unstagedPhpFiles[@]} -gt 0 ]]; then
+        echo "${cBlue} Stashing unstaged changes from ${#unstagedPhpFiles[@]} PHP files${cEnd}"
+        git stash push --keep-index -m PRECOMMIT
+        stashed=true
     fi
+    # Then do a dry-run on staged files and exit if clean (real fix has unreliable exit codes)
+    if $csFixerBin fix --dry-run --config=build/php-cs-fixer/config.php; then
+        echo "${cGreen} Nothing to fix!${cEnd}"
+        [ $stashed = true ] && git stash pop --quiet
+        exit 0
+    fi
+
+    # Now fix for real, and add the safe files.
+    echo "${cGreen} Fixing files...${cEnd}"
+    result=$( $csFixerBin fix --config=build/php-cs-fixer/config.php )
+    echo "$result" | sed -e "s/\(.*\)/$cBlue\1$cEnd/g; s/\+  /$cGreen+  /g; s/-  /$cRed-  /g;"
+    for file in "${safeFiles[@]}"; do
+        echo "${cOrange} Adding fixes from ${file}${cEnd}"
+        git add "${file}"
+    done
+
+    # Finally reset the rest and pop the stash
+    git checkout -- .
+    [ $stashed = true ] && git stash pop --quiet
+
+    # ...before fixing one more time, but on all files, for a manual git add -p
+    result=$( $csFixerBin fix --config=build/php-cs-fixer/config.php )
+    echo "$result" | sed -e "s/\(.*\)/$cBlue\1$cEnd/g; s/\+  /$cGreen+  /g; s/-  /$cRed-  /g;"
+
+    echo "${cRed} Some files were fixed but could not be added automatically.${cEnd}"
+    echo "${cOrange}  Review the changes manually then re-commit with${cEnd}"
+    echo "${cOrange}   git commit -e --file .git/COMMIT_EDITMSG${cEnd}"
+    exit 3
 fi
