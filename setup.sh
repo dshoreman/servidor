7980b1ca79d6817547510e6e7ad344445deb483f
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-05-27 01:27:57 +0100

    Fix php fpm socket in nginx config

--- setup.sh
+++ setup.sh
@@ -355,7 +355,7 @@ server {
         try_files \$uri \$uri/ /index.php?\$query_string;
     }
     location ~ \.php\$ {
-        fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+        fastcgi_pass unix:/run/php/php8.0-fpm.sock;
         fastcgi_index index.php;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         include fastcgi_params;
