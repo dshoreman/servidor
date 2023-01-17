9d11b27cede85c2fc4097d554b21258447a48a2f
Dave Shoreman <dshoreman@users.noreply.github.com>
2023-01-17 20:45:09 +0000

    Add an SSL status badge/label to project apps
    Uses a new `SslIndicator` component to add a label alongside the
    php/language version indicator in the Source Files header bar.
    
    The lock icon is open or closed based on SSL being enabled/disabled.

---
+++ resources/js/components/Projects/Apps/SslIndicator.vue
@@ -0,0 +1,31 @@
+<template>
+    <sui-label size="tiny" :color="txt(...colours)" :title="txt(...details)">
+        <sui-icon :name="txt(...icons)" /> {{ txt(...statuses) }}
+    </sui-label>
+</template>
+
+<script>
+export default {
+    props: {
+        app: { type: Object, default: () => ({}) },
+    },
+    data: () => ({
+        icons: ['lock', 'unlock'],
+        colours: ['green', 'red'],
+        statuses: ['SSL Enabled', 'SSL Disabled'],
+        details: [
+            'SSL is enabled!',
+            'SSL is not enabled. Data may be insecure.',
+        ],
+    }),
+    methods: {
+        txt(good, bad) {
+            if (this.app.config.ssl) {
+                return good;
+            }
+
+            return bad;
+        },
+    },
+};
+</script>
